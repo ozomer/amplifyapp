5b61b5b7ef674f56a637c9fa8b704ad4c98364f1
Oren Zomer <oren@awearsolutions.com>
2019-12-15 14:22:37 +0200

    using a counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => setCount(count + 1)}>Click Me</button>
+        <span>{count}</span>
       </header>
     </div>
   );
